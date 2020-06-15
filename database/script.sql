221b22646e182b4422627393cb4b0677aa1fd922
rafaellevissa <rafaellevissa@gmail.com>
2020-06-15 15:02:07 -0300

    :art: banco

---
+++ database/script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE concessionaria DEFAULT CHARACTER SET utf8;
+USE concessionaria;
+
+CREATE TABLE Motores (
+    MotorID INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    potencia VARCHAR(3),
+    qntCilindros INTEGER
+);
+CREATE TABLE Carros (
+    chassi INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR (255),
+    modelo VARCHAR (255),
+    cor VARCHAR (255),
+    MotorID INTEGER NOT NULL,
+    FOREIGN KEY (MotorID) REFERENCES Motores(MotorID)
+);
