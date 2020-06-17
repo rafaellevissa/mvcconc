ccbe715de01f2901e4220b46b2bf1d2cea3ef265
rafaellevissa <rafaellevissa@gmail.com>
2020-06-17 15:00:47 -0300

    :art: controller de criar

--- src/controllers/controllerCreateMotor.php
+++ src/controllers/controllerCreateMotor.php
@@ -1,7 +1,23 @@
 <?php
-
+require_once '../models/motor.php';
 class ControllerCreateMotor{
+    private $oMotor;
     public function __construct(){
+        $this->oMotor = new Motor();
+        $this->cadastrar();
+    }
 
+    public function cadastrar(){
+        $this->oMotor->setMarca($_POST['potencia']);
+        $this->oMotor->setModelo($_POST['qntCilindros']);
+        $this->oMotor->setCor($_POST['chassi']);
+
+        $resultado = $this->oMotor->incluir();
+        if($resultado>=1){
+            echo("<script>alert('Motor cadastrado com sucesso!');document.location='../views/cadastroMotor.php'</script>");
+        }else{
+            echo("<script>alert('Infelizmente não foi possível cadastrar');history.back()</script>");
+        }
     }
 }
+new ControllerCreateMotor();
